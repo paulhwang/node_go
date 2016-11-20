5e3fa62e2a9d853ed7a663e41cf763c873d91b65
paul.hwang (黃文樸 - Getac) <paul.hwang@getac.com.tw>
2016-11-21 02:18:36 +0800

    log

--- phwang_modules/util_modules/queue_module.js
+++ phwang_modules/util_modules/queue_module.js
@@ -225,11 +225,11 @@ function QueueObject (root_object_val) {
     };
 
     this.logit = function (str1_val, str2_val) {
-        require("../root_module.js").LOG_IT(this.objectName() + "." + str1_val, str2_val);
+        this.rootObject().LOG_IT(this.objectName() + "." + str1_val, str2_val);
     };
 
     this.abend = function (str1_val, str2_val) {
-        require("../root_module.js").ABEND(this.objectName() + "." + str1_val, str2_val);
+        this.rootObject().ABEND(this.objectName() + "." + str1_val, str2_val);
     };
 
     this.init__(root_object_val);

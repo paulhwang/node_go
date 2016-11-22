48dbbab70a65700983cdbad54426b7ed5b0ad0a4
Paul Hwang <paul.hwang@getac.com.tw>
2016-11-22 19:06:27 +0800

    name

---
+++ phwang_modules/fabric_modules/cluster.js
@@ -0,0 +1,195 @@
+/*
+ * Copyrights phwang
+ * Written by Paul Hwang
+ * File name: go_cluster_module.js
+ */
+
+module.exports = {
+    malloc: function (cluster_mgr_val, topic_data_val, session_val) {
+        return new clusterObject(cluster_mgr_val, topic_data_val, session_val);
+    },
+};
+
+function clusterObject (cluster_mgr_val, topic_data_val, session_val) {
+    "use strict";
+
+    this.init__ = function (cluster_mgr_val, topic_data_val, session_val) {
+        this.theClusterMgrObject = cluster_mgr_val;
+        session_val.setClusterObject(this);
+        this.theSessionArray = [2];
+        this.theSessionArray[0] = session_val;
+        this.theSessionArrayLength = 1;
+        this.theReceiveQueue = this.rootObject().mallocQueue();
+        this.theTransmitQueue = this.rootObject().mallocQueue();
+        this.theNext = null;
+        this.thePrev = null;
+        this.createTopic(topic_data_val);
+        this.debug(false, "init__", "topic=" + this.topicObject().objectName());
+    };
+
+    this.goObjectMalloc = function () {
+        var go_module = require("./../go_modules/go_base.js")
+        return go_module.malloc(this);
+    },
+
+    this.objectName = function () {
+        return "clusterObject";
+    };
+
+    this.sessionObject = function () {
+        return this.sessionArray(0);
+    };
+
+    this.clusterMgrObject = function () {
+        return this.theClusterMgrObject;
+    };
+
+    this.rootObject = function () {
+        return this.clusterMgrObject().rootObject();
+    };
+
+    this.fabricObject = function () {
+        return this.clusterMgrObject().fabricObject();
+    };
+
+    this.utilObject = function () {
+        return this.clusterMgrObject().utilObject();
+    };
+
+    this.topicObject = function () {
+        return this.theTopicObject;
+    };
+
+    this.setTopicObject = function (val) {
+        this.theTopicObject = val;
+    };
+
+    this.sessionArray = function (index_val) {
+        return this.theSessionArray[index_val];
+    };
+
+    this.sessionArrayLength = function () {
+        return this.theSessionArrayLength;
+    };
+
+    this.incrementSessionArrayLength = function () {
+        this.theSessionArrayLength += 1;
+    };
+
+    this.prev = function () {
+        return this.thePrev;
+    };
+
+    this.setPrev = function (val) {
+        this.thePrev = val;
+    };
+
+    this.next = function () {
+        return this.theNext;
+    };
+
+    this.setNext = function (val) {
+        this.theNext = val;
+    };
+
+    this.receiveQueue = function () {
+        return this.theReceiveQueue;
+    };
+
+    this.transmitQueue = function () {
+        return this.theTransmitQueue;
+    };
+
+    this.createTopic = function (topic_data_val) {
+        var topic_data = JSON.parse(topic_data_val);
+        if (topic_data.title === "go") {
+            this.setTopicObject(this.goObjectMalloc());
+        }
+    };
+
+    this.addAdditionalSession = function (session_val) {
+        this.theSessionArray[this.sessionArrayLength()] = session_val;
+        this.incrementSessionArrayLength();
+    };
+
+    this.enqueueTransmitData = function (data_val) {
+        this.debug(false, "enqueueTransmitData", data_val);
+        this.transmitQueue().enQueue(data_val);
+    };
+
+    this.dequeueTransmitData = function () {
+        var data = this.transmitQueue().deQueue();
+        this.debug(false, "dequeueTransmitData", data);
+        return data;
+    };
+
+    this.enqueueReceiveData = function (data_val) {
+        this.debug(false, "enqueueReceiveData", data_val);
+        this.receiveQueue().enQueue(data_val);
+    };
+
+    this.dequeueReceiveData = function () {
+        var data = this.receiveQueue().deQueue();
+        this.debug(false, "dequeueReceiveData", data);
+        return data;
+    };
+
+    this.processTransmitData = function () {
+        while (true) {
+            var data = this.dequeueTransmitData();
+            if (!data) {
+                return;
+            }
+
+            var i = 0;
+            while (i < this.sessionArrayLength()) {
+                this.sessionArray(i).enqueueTransmitData(data);
+                i += 1;
+            }
+        }
+    };
+
+    this.processSetupTopicData = function (json_data_val) {
+        this.debug(true, "processSetupTopicData", "data=" + json_data_val);
+        var topic_data = JSON.parse(json_data_val);
+        if (topic_data.command === "config") {
+            this.topicObject().configObject().createConfig(topic_data.data);
+        }
+    };
+
+    this.processReceiveData = function () {
+        while (true) {
+            var data = this.dequeueReceiveData();
+            if (!data) {
+                return;
+            }
+            this.receiveStringData(data);
+        }
+    };
+
+    this.enqueAndPocessReceiveData = function (data_val) {
+        this.debug(false, "enqueAndPocessReceiveData", data_val);
+        this.enqueueReceiveData(data_val);
+        this.processReceiveData();
+    };
+
+    this.receiveStringData = function (str_val) {
+        this.topicObject().portObject().receiveStringData(str_val);
+    };
+
+    this.debug = function (debug_val, str1_val, str2_val) {
+        if (debug_val) {
+            this.logit(str1_val, str2_val);
+        }
+    };
+
+    this.logit = function (str1_val, str2_val) {
+        this.rootObject().LOG_IT(this.objectName() + "." + str1_val, str2_val);
+    };
+
+    this.abend = function (str1_val, str2_val) {
+        this.rootObject().ABEND(this.objectName() + "." + str1_val, str2_val);
+    };
+
+    this.init__(cluster_mgr_val, topic_data_val, session_val);
+}
---
+++ phwang_modules/fabric_modules/link_mgr.js
@@ -0,0 +1,243 @@
+/*
+ * Copyrights phwang
+ * Written by Paul Hwang
+ * File name: link_mgr_module.js
+ */
+
+module.exports = {
+    malloc: function (root_object_val) {
+        return new LinkMgrObject(root_object_val);
+    },
+};
+
+function LinkMgrObject(root_object_val) {
+    "use strict";
+
+    this.init__ = function (root_object_val) {
+        this.theRootObject = root_object_val;
+        this.theGlobalLinkId = 10;
+        this.theHead = null;
+        this.theTail = null;
+        this.theSize = 0;
+        this.theNameListChanged = false;
+        this.debug(false, "init__", "");
+    };
+
+    this.linkModuleMalloc = function (my_name_val, link_id_val) {
+        var link_module = require("./link.js");
+        return link_module.malloc(this, my_name_val, link_id_val);
+    };
+
+    this.objectName = function () {
+        return "LinkMgrObject";
+    };
+
+    this.rootObject = function () {
+        return this.theRootObject;
+    };
+
+    this.utilObject = function () {
+        return this.rootObject().utilObject();
+    };
+
+    this.globalLinkId = function () {
+        return this.theGlobalLinkId;
+    };
+
+    this.incrementGlobalLinkId = function () {
+        this.theGlobalLinkId += 1;
+    };
+
+    this.head = function () {
+        return this.theHead;
+    }
+
+    this.setHead = function (val) {
+        this.theHead = val;
+    }
+
+    this.tail = function () {
+        return this.theTail;
+    }
+
+    this.setTail = function (val) {
+        this.theTail = val;
+    }
+
+    this.size = function () {
+        return this.theSize;
+    }
+
+    this.incrementSize = function () {
+        this.theSize += 1;
+    }
+
+    this.decrementSize = function () {
+        this.theSize -= 1;
+    }
+
+    this.mallocLink = function (my_name_val) {
+        var link = this.linkModuleMalloc(my_name_val, this.globalLinkId());
+        this.incrementGlobalLinkId();
+        this.insertLinkToList(link);
+        this.setNameListChanged();
+        return link;
+    };
+
+    this.freeLink = function (link_val) {
+        this.deleteLinkFromList(link_val);
+    };
+
+    this.insertLinkToList = function (link_val) {
+        if (!link_val) {
+            this.abend("insertLinkToList", "null link_val");
+            return;
+        }
+
+        this.abendIt();
+
+        this.incrementSize();
+        if (!this.head()) {
+            link_val.setPrev(null);
+            link_val.setNext(null);
+            this.setHead(link_val);
+            this.setTail(link_val);
+        } else {
+            this.tail().setNext(link_val);
+            link_val.setPrev(this.tail());
+            link_val.setNext(null);
+            this.setTail(link_val);
+        }
+        this.abendIt();
+    };
+
+    this.deleteLinkFromList = function (link_val) {
+        if (this.size() <= 0) {
+            this.abend("deleteLinkFromList", "size=" + this.size());
+            return;
+        }
+        if (!this.linkExistInTheList(link_val)) {
+            this.abend("deleteLinkFromList", "linkExistInTheList is false");
+            return;
+        }
+
+        this.abendIt();
+        if (link_val.prev()) {
+            link_val.prev().setNext(link_val.next());
+        } else {
+            this.setHead(link_val.next());
+        }
+        if (link_val.next()) {
+            link_val.next().setPrev(link_val.prev());
+        } else {
+            this.setTail(link_val.prev());
+        }
+        this.decrementSize();
+        this.abendIt();
+    };
+
+    this.searchLinkByNameAndLinkId = function (my_name_val, link_id_val) {
+        this.debug(false, "searchLinkByNameAndLinkId", my_name_val + " " + link_id_val);
+        var link = this.head();
+        while (link) {
+            if ((link.linkId() === link_id_val) && (link.myName() === my_name_val)) {
+                return link;
+            }
+            link = link.next();
+        }
+        return null;
+    };
+    
+    this.searchLinkByLinkId = function (link_id_val) {
+        this.debug(false, "searchLinkByLinkId", link_id_val);
+        var link = this.head();
+        while (link) {
+            if (link.linkId() === link_id_val) {
+                return link;
+            }
+            link = link.next();
+        }
+        return null;
+    };
+
+    this.searchLinkByName = function (my_name_val) {
+        this.debug(false, "searchLinkByName", my_name_val);
+        var link = this.head();
+        while (link) {
+            if (link.myName() === my_name_val) {
+                return link;
+            }
+            link = link.next();
+        }
+        return null;
+    };
+
+    this.linkExistInTheList = function (link_val) {
+        var link = this.head();
+        while (link) {
+            if (link === link_val) {
+                return true;
+            }
+            link = link.next();
+        }
+        return false;
+    };
+
+    this.setNameListChanged = function () {
+        var link = this.head();
+        while (link) {
+            link.setNameListChanged();
+            link = link.next();
+        }
+    };
+
+    this.getNameList = function () {
+        var name_array = [];
+        var i = 0;
+        var link = this.head();
+        while (link) {
+            name_array[i] = link.myName();
+            i += 1;
+            link = link.next();
+        }
+        return name_array;
+    };
+
+    this.abendIt = function () {
+        var i = 0;
+        var link = this.head();
+        while (link) {
+            link = link.next();
+            i += 1;
+        }
+        if (i !== this.size()) {
+            this.abend("abendIt", "head: size=" + this.size() + " i=" + i);
+        }
+
+        i = 0;
+        link = this.tail();
+        while (link) {
+            link = link.prev();
+            i += 1;
+        }
+        if (i !== this.size()) {
+            this.abend("abendIt", "tail: size=" + this.size() + " i=" + i);
+        }
+    };
+
+    this.debug = function (debug_val, str1_val, str2_val) {
+        if (debug_val) {
+            this.logit(str1_val, str2_val);
+        }
+    };
+
+    this.logit = function (str1_val, str2_val) {
+        this.rootObject().LOG_IT(this.objectName() + "." + str1_val, str2_val);
+    };
+
+    this.abend = function (str1_val, str2_val) {
+        this.rootObject().ABEND(this.objectName() + "." + str1_val, str2_val);
+    };
+
+    this.init__(root_object_val);
+}

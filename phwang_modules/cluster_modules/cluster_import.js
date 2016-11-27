86e4ea9a9457fcab59ba112dace8ee99d02e01f2
Paul Hwang <phwang@gmail.com>
2016-11-27 10:50:27 +0800

    import

---
+++ phwang_modules/cluster_modules/cluster_import.js
@@ -0,0 +1,41 @@
+/*
+ * Copyrights phwang
+ * Written by Paul Hwang
+ * File name: import.js
+ */
+
+module.exports = {
+    malloc: function (root_object_val) {
+        return new ClusterImportClass(root_object_val);
+    },
+};
+
+function ClusterImportClass (root_object_val) {
+    "use strict";
+
+    this.init__ = function (root_object_val) {
+        this.theClusterRootObject = root_object_val;
+    };
+
+    this.clusterRootObject = function () {
+        return this.theClusterRootObject;
+    };
+
+    this.rootObject = function () {
+        return this.clusterRootObject().rootObject();
+    };
+
+    this.importLogit = function () {
+        return require("../util_modules/logit.js");
+    }
+
+    this.mallocQueue = function () {
+        return require("../util_modules/queue.js").malloc(this.rootObject());
+    };
+
+    this.mallocRing = function () {
+        return require("../util_modules/ring.js").malloc(this.rootObject());
+    };
+
+    this.init__(root_object_val);
+};

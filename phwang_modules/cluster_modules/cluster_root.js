cd0bff69783dfa58008e41190d48f02f0b2c9fcd
Paul Hwang <phwang@gmail.com>
2016-11-27 11:00:29 +0800

    import

--- phwang_modules/cluster_modules/cluster_root.js
+++ phwang_modules/cluster_modules/cluster_root.js
@@ -16,12 +16,8 @@ function ClusterRootClass (root_object_val) {
     this.init__ = function (root_object_val) {
         this.theRootObject = root_object_val;
         this.theImportObject = require("./cluster_import.js").malloc(this);
-        //this.theBaseObject = this.importObject().importBase().malloc(this);
-        //this.theLinkMgrObject = this.importObject().importListMgr().malloc_mgr(this, 0);
         this.theClusterBaseObject = this.importObject().importClusterBase().malloc(this);
         this.theClusterMgrObject = this.importObject().importListMgr().malloc_mgr(this, 0);
-        //this.theSwitchObject = this.importObject().importSwitch().malloc(this);
-        //this.theAjaxObject = this.importObject().importAjax().malloc(this);
         this.debug(true, "init__", "");
     };
 
@@ -41,24 +37,12 @@ function ClusterRootClass (root_object_val) {
         return this.theBaseObject;
     };
 
-    this.linkMgrObject = function () {
-        return this.theLinkMgrObject;
-    };
-
     this.clusterBaseObject = function () {
         return this.theClusterBaseObject;
     };
 
     this.clusterMgrObject = function () {
         return this.theClusterMgrObject;
-    };
-
-    this.switchObject = function () {
-        return this.theSwitchObject;
-    };
-
-    this.ajaxObject = function () {
-        return this.theAjaxObject;
     };
 
     this.topicMallocBase = function () {
@@ -71,26 +55,6 @@ function ClusterRootClass (root_object_val) {
 
     this.topicTransmitData = function (base_id_val) {
         return require("../go_modules/go_root.js").transmit_data(base_id_val);
-    };
-
-    this.processPost = function (req, res) {
-        this.ajaxObject().processPost(req, res);
-    };
-
-    this.processGet = function (req, res) {
-        this.ajaxObject().processGet(req, res);
-    };
-
-    this.processNotFound = function (req, res) {
-        console.log(req.headers);
-        this.debug(true, "processNotFound", "*****");
-        res.type('text/plain');
-        res.status(404);
-        res.send('Not Found');
-    };
-
-    this.processFailure = function (err, req, res, next) {
-        this.logit("processFailure", state);
     };
 
     this.debug = function (debug_val, str1_val, str2_val) {

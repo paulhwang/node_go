51beb7bfb7aff99aad164508aed4d3efc04daa19
Paul Hwang <paul.hwang@getac.com.tw>
2016-11-28 16:08:39 +0800

    matrix

--- phwang.js
+++ phwang.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var matrix_root = require('./phwang_modules/matrix_modules/matrix_root.js');
-var root = require('./phwang_modules/fabric_modules/fabric_root.js');
+var fabric_root = require('./phwang_modules/fabric_modules/fabric_root.js');
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/../js_go"));
-app.post("/django_go/go_ajax/", root.post);
-app.get("/django_go/go_ajax/", root.get);
-app.use(root.not_found);
-app.use(root.failure);
+app.post("/django_go/go_ajax/", fabric_root.post);
+app.get("/django_go/go_ajax/", fabric_root.get);
+app.use(fabric_root.not_found);
+app.use(fabric_root.failure);
 app.listen(8080);

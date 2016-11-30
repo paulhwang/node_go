641edc4b3c4a14ef3c6995385439bbc975ed4411
Paul Hwang <paul.hwang@getac.com.tw>
2016-11-30 16:11:28 +0800

    matrix

--- phwang_modules/util_modules/queue.js
+++ phwang_modules/util_modules/queue.js
@@ -16,18 +16,10 @@ function QueueClass (root_object_val) {
     this.init__ = function (root_object_val) {
         this.theRootObject = root_object_val;
         this.theListObject = require("./list_mgr.js").malloc_mgr(this, 0);
-        this.theHead = null;
-        this.theTail = null;
-        this.theSize = 0;
         this.debug(true, "init__", this.rootObject().objectName());
         if (this.debugRing()) {
             this.theRingObject = this.rootObject().importObject().mallocRing();
         }
-        this.debug(true, "init__", "");
-    };
-
-    this.debugMe = function () {
-        return true;
     };
 
     this.debugRing = function () {
@@ -54,32 +46,8 @@ function QueueClass (root_object_val) {
         return this.theListObject;
     };
 
-    this.head = function () {
-        return this.theHead;
-    }
-
-    this.setHead = function (val) {
-        this.theHead = val;
-    }
-
-    this.tail = function () {
-        return this.theTail;
-    }
-
-    this.setTail = function (val) {
-        this.theTail = val;
-    }
-
     this.size = function () {
-        return this.theSize;
-    }
-
-    this.incrementSize = function () {
-        this.theSize += 1;
-    }
-
-    this.decrementSize = function () {
-        this.theSize -= 1;
+        return this.listObject().size();
     }
 
     this.enQueue = function (data_val) {
@@ -96,64 +64,20 @@ function QueueClass (root_object_val) {
             }
         }
 
-        this.abendIt();
-
-        var data_entry = new HolderEntryClass(this, this.listObject().allocId());
-        if (!data_entry) {
-            this.abend("enQueue", "null data_entry");
-            return;
-        }
-        data_entry.setData(data_val);
-        this.listObject().enQueue(data_entry);
-        return;
-
-        this.incrementSize();
-        if (!this.head()) {
-            data_entry.setPrev(null);
-            data_entry.setNext(null);
-            this.setHead(data_entry);
-            this.setTail(data_entry);
-        } else {
-            this.tail().setNext(data_entry);
-            data_entry.setPrev(this.tail());
-            data_entry.setNext(null);
-            this.setTail(data_entry);
-        }
-        this.abendIt();
+        var entry = new HolderEntryClass(this, this.listObject().allocId());
+        entry.setData(data_val);
+        this.listObject().enQueue(entry);
     };
 
     this.deQueue = function () {
         var data_entry;
-        var data;
 
         data_entry = this.listObject().deQueue();
         if (!data_entry) {
             this.debug(false, "deQueue", "empty: " + this.rootObject().objectName());
             return null;
         }
-        return data_entry.data();
-
-        this.abendIt();
-
-        if (!this.head()) {
-            data_entry = null;
-            data = null;
-        } else if (this.head() === this.tail()) {
-            this.decrementSize();
-            data_entry = this.head();
-            data = data_entry.data();
-            this.setHead(null);
-            this.setTail(null);
-        } else {
-            this.decrementSize();
-            data_entry = this.head();
-            data = data_entry.data();
-            this.setHead(this.head().next());
-            this.head().setPrev(null);
-        }
-
-        this.abendIt();
-
+        var data = data_entry.data();
 
         if (data && this.debugRing()) {
             var i = 10
@@ -170,73 +94,6 @@ function QueueClass (root_object_val) {
         }
 
         return data;
-    };
-
-    this.unQueue = function (func_val, input_val1, input_val2, input_val3) {
-        this.abendIt();
-
-        var p = this.head();
-        while (p) {
-            this.debug(false, "unQueue", "in while loop");
-            if (func_val(p.data(), input_val1, input_val2, input_val3)) {
-                this.debug(false, "unQueue", "found");
-                if (p.prev()) {
-                    p.prev().setNext(p.next());
-                } else {
-                    this.setHead(p.next());
-                }
-                if (p.next()) {
-                    p.next().setPrev(p.prev());
-                } else {
-                    this.setTail(p.prev());
-                }
-                this.decrementSize();
-                return p;
-            }
-            p = p.next();
-        }
-        this.abendIt();
-        this.debug(false, "unQueue", "not found");
-    };
-
-    this.searchIt = function (func_val, input_val1, input_val2, input_val3) {
-        var p = this.head();
-        while (p) {
-            this.debug(false, "searchIt", "in while loop");
-            if (func_val(p.data(), input_val1, input_val2, input_val3)) {
-            this.debug(false, "searchIt", "found");
-                return p.data();
-            }
-            p = p.next();
-        }
-        this.debug(false, "searchIt", "not found");
-        return null;
-    };
-
-    this.abendIt = function () {
-        if (!this.debugMe()) {
-            return;
-        }
-
-        var i = 0;
-        var p = this.head();
-        while (p) {
-            p = p.next();
-            i += 1;
-        }
-        if (i !== this.size()) {
-            this.abend("abendIt", "head: size=" + this.size() + " i=" + i);
-        }
-
-        i = 0;
-        p = this.tail();
-        while (p) {
-            p = p.prev();
-            i += 1;
-        }
-        if (i !== this.size()) {
-            this.abend("abendIt", "tail: size=" + this.size() + " i=" + i);
-        }
     };
 
     this.debug = function (debug_val, str1_val, str2_val) {

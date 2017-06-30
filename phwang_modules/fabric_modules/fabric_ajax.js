3fc5cee15633d8b23f363b37025a80832e3abf0b
paulhwang <phwang@gmail.com>
2017-06-30 09:42:07 +0000

    t

--- phwang_modules/fabric_modules/fabric_ajax.js
+++ phwang_modules/fabric_modules/fabric_ajax.js
@@ -86,6 +86,8 @@ function FabricAjaxClass(root_object_val) {
 
         if (!this.ajaxParserObject().useLinkMgrService() 
             && (go_request.command !== "setup_link")
+            //&& (go_request.command !== "get_link_data")
+            && (go_request.command !== "get_name_list")
             && (go_request.command !== "setup_session")
             && (go_request.command !== "put_session_data")) {
             this.sendHttpResponse(go_request, res, data);

e9325a858bcdf82e2b332d653046b3368499961a
paulhwang <phwang@gmail.com>
2017-07-05 09:25:17 +0000

    t

--- phwang_modules/fabric_modules/link_mgr_service.js
+++ phwang_modules/fabric_modules/link_mgr_service.js
@@ -96,7 +96,7 @@ function LinkMgrServiceClass(root_object_val) {
         this.callbackFunc = callback_func_val;
         this.theGoRequest = go_request_val;
         this.theRes = res_val;
-        this.netClientOjbect().write("G" +  link_id_index_val);
+        this.netClientOjbect().write("D" +  link_id_index_val);
     };
 
     this.getNameList = function (link_id_index_val, name_list_tag_val, callback_func_val, go_request_val, res_val) {
@@ -125,7 +125,10 @@ function LinkMgrServiceClass(root_object_val) {
 
     this.getSessionData = function (link_id_index_val, session_id_index_val, callback_func_val, go_request_val, res_val) {
         this.debug(true, "getSessionData", "link_id_index_val=" + link_id_index_val + " session_id_index_val=" + session_id_index_val);
-
+        this.callbackFunc = callback_func_val;
+        this.theGoRequest = go_request_val;
+        this.theRes = res_val;
+        this.netClientOjbect().write("G" + link_id_index_val + session_id_index_val);
     };
 
     this.putSessionData = function (link_id_index_val, session_id_index_val, data_val, callback_func_val, go_request_val, res_val) {
@@ -134,7 +137,6 @@ function LinkMgrServiceClass(root_object_val) {
         this.theGoRequest = go_request_val;
         this.theRes = res_val;
         this.netClientOjbect().write("P" + link_id_index_val + session_id_index_val + data_val);
-
     };
 
 
